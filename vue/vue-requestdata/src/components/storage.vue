d68ec966d0755ad0fae8f4e0024d3363fdc3bc1e
953708519 <39235365+953708519@users.noreply.github.com>
2018-12-12 10:36:51 +0800

    vue-todo
    封装sessionstorage

---
+++ vue/vue-requestdata/src/components/storage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="title">
+      todo，刷新不会重新加载
+    </div>
+  <input type="text" v-model="todo">
+  <button @click="add()">增加</button>
+  <h2>进行中</h2>
+  <div v-for="(item,key) in list" v-if='!item.check' >
+     <input type="checkbox" v-model="item.check" >  
+          {{item.title}}
+     <button @click="dele(key)">减少</button>
+  </div>
+  <h2>已完成</h2>
+  <div v-for="(item,key) in list" v-if='item.check' >
+    <input type="checkbox" v-model="item.check" @change="change()" >  
+          {{item.title}}
+    <button @click="dele(key)">减少</button></div>
+  </div>
+</template>
+<script>
+import storage from '../model/storage.js';
+
+  export default {
+    name:'storage',
+    data(){
+      return {
+      list:[],
+      todo:'',
+      }
+  },
+      methods:{
+       add(){
+         if(!this.todo){
+           alert('请输入');
+         }
+         else{
+        this.list.push({title:this.todo,check:false});
+        this.todo="";
+     
+        storage.set('list',this.list);
+        }
+       },
+       dele(key){
+        this.list.splice(key,1);
+         storage.set('list',this.list);
+       },
+       change(){
+         storage.set('list',this.list);
+       }
+      },
+      mounted(){
+        var list=storage.get('list');
+        if(list){
+          this.list=list;
+          console.log(this.list)
+        }
+      }
+    }
+</script>
+<style >
+.red{color:red;}
+.title{
+  font-size: 20px;
+  font-weight: bolder;
+   padding-bottom: 20px;
+  color: red;
+}
+</style>
