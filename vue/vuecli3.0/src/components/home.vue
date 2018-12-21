11bef67359a711fb7b75d8836e0536d9c0e5774c
953708519 <39235365+953708519@users.noreply.github.com>
2018-12-21 17:53:38 +0800

    vue-router使用
    定义router.js

---
+++ vue/vuecli3.0/src/components/home.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+       欢迎学习VUE 
+       <br>
+    <router-link to='/requestdata'>请求数据</router-link>
+    <br>
+    <router-link to='/storage'>TODO例子</router-link>
+     <br>
+    <router-link to='/parent'>传值</router-link>
+    </div>
+    
+</template>
+<script>
+export default {
+    name:'home'
+}
+</script>
---
+++ vue/vuecli3.0/src/router.js
@@ -0,0 +1,88 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './components/home.vue'
+import requestdata from './components/requestdata.vue'
+import storage from './components/storage.vue'
+import parent from './components/transdata/parent.vue'
+
+Vue.use(Router)
+export default new Router({
+      routes:[ 
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path:'/',
+            redirect:'/home'
+        },
+        {
+            path: '/requestdata',
+            component: requestdata
+        },
+        {
+            path: '/storage',
+            component: storage
+        },
+        {
+            path: '/parent',
+            component: parent
+        }
+     ]
+})
+
+/*
+vue路由配置：
+
+
+	1.安装 
+
+	npm install vue-router  --save   / cnpm install vue-router  --save
+
+
+    2、引入并 Vue.use(VueRouter)   (router.js)
+         import Vue from 'vue'
+ 
+		import VueRouter from 'vue-router'
+
+		Vue.use(VueRouter)
+
+	
+	3、配置路由
+
+		
+
+		1、创建组件 引入组件 router.js
+
+
+		2、定义路由  实例化
+
+			routes = [
+			  { path: '/foo', component: Foo },
+			  { path: '/bar', component: Bar },
+			  { path: '*', redirect: '/home' }   
+			]
+
+    
+            4、挂载 main.js中引入router.js
+    
+              import router from './router.js'      
+            new Vue({
+              el: '#app',
+              router，
+              render: h => h(App)
+            })
+    
+    
+            
+            5 、根组件的模板里面放上这句话   
+            <router-view></router-view>         
+    
+    
+    
+    
+            6、路由跳转
+            <router-link to="/requestdata">Go to Foo</router-link>
+             <router-link to="/storage">storage</router-link>
+
+*/
