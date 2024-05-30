1b865e426e796182943aa1de95a7c853133c9e29
Bruce Lee <bruce.withworld@gmail.com>
2024-05-30 17:51:30 +0800

    the day 8 of algorithm

---
+++ Algorithm/HashTable/242.valid-anagram.js
@@ -0,0 +1,23 @@
+//Link https://leetcode.cn/problems/valid-anagram/description/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) return false;
+
+    let charMap = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        charMap.set(s[i], (charMap.get(s[i]) || 0) + 1);
+        charMap.set(t[i], (charMap.get(t[i]) || 0) - 1);
+    }
+
+    for (const count of charMap.values()) {
+        if (count !== 0) return false;
+    }
+
+    return true;
+};
