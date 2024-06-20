ff28d0f13c7899e3a44142c51b011a547190df6c
Bruce Lee <bruce.withworld@gmail.com>
2024-06-20 14:51:38 +0800

    flatArray

---
+++ Algorithm/NormalJs/flatArray.js
@@ -0,0 +1,46 @@
+/**
+ * @param {Array} arr
+ * @return {Array}
+ */
+var flatArray = function (arr) {
+    var result = []
+  
+    var flat = function(items){
+     items.forEach(item=>{
+      if(Array.isArray(item)){
+        flat(item)
+      }else{
+        result.push(item)
+      }
+     })
+    }
+  
+    flat(arr)
+  
+    return result
+  };
+
+  
+  /**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    var result = []
+
+
+    var flatArray = function (items, depth) {
+        items.forEach(item => {
+            if (Array.isArray(item) && depth > 0) {
+                flatArray(item, depth - 1)
+            } else {
+                result.push(item)
+            }
+        })
+    }
+
+    flatArray(arr, n)
+
+    return result
+};
