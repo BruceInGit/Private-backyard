877189bb23dd42bb662ee0133a510612e66cd4d6
Bruce Lee <bruce.withworld@gmail.com>
2025-02-27 18:27:19 +0800

    insertionSort

--- Algorithm/NormalJs/sort.js
+++ Algorithm/NormalJs/sort.js
@@ -1,24 +1,24 @@
 // quick sort
-function quickSort(arr){
+function quickSort(arr) {
     if (arr.length <= 1) return arr;
-    let left=[]
-    let right=[]
+    let left = []
+    let right = []
     const base = arr[0]
-   for(let i=1;i<arr.length;i++){
-    if (arr[i] < base) {
-        left.push(arr[i])
-    }else{
-         right.push(arr[i])
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < base) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
     }
-   }
 
-   return [...quickSort(left),base,...quickSort(right)]
+    return [...quickSort(left), base, ...quickSort(right)]
 }
 
 // bubble sort
 function bubbleSort(arr) {
     let len = arr.length;
-    for (let i = 0; i < len - 1; i++) { 
+    for (let i = 0; i < len - 1; i++) {
         let swapped = false
         for (let j = 0; j < len - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -33,18 +33,57 @@ function bubbleSort(arr) {
     return arr;
 }
 
-function bubbleSortB(arr){
-   let swapped
-  
-  do {
-    swapped = false
-    for (let index = 0; index < arr.length -1; index++) {
-        if (arr[index] > arr[index + 1]) {
-            [arr[index], arr[index + 1]] = [arr[index + 1], arr[index]];
-            swapped = true
+function bubbleSortB(arr) {
+    let swapped
+
+    do {
+        swapped = false
+        for (let index = 0; index < arr.length - 1; index++) {
+            if (arr[index] > arr[index + 1]) {
+                [arr[index], arr[index + 1]] = [arr[index + 1], arr[index]];
+                swapped = true
+            }
         }
-       }
-   }while(swapped)
-   
+    } while (swapped)
+
+    return arr
+}
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let numberToInsert = arr[i]
+    }
+}
+return arr;
+}
+
+function bubbleSortB(arr) {
+    let swapped
+
+    do {
+        swapped = false
+        for (let index = 0; index < arr.length - 1; index++) {
+            if (arr[index] > arr[index + 1]) {
+                [arr[index], arr[index + 1]] = [arr[index + 1], arr[index]];
+                swapped = true
+            }
+        }
+    } while (swapped)
+
+    return arr
+}
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let numberToInsert = arr[i]
+        let j = i - 1
+
+        while (j >= 0 && arr[j] > numberToInsert) {
+            arr[j + 1] = arr[j]
+            j--
+        }
+        arr[j+1] = numberToInsert
+    }
+
     return arr
 }
