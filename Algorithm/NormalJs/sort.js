286933eb70e0e27b1863a9b242aefda6f559e5e1
Bruce Lee <bruce.withworld@gmail.com>
2025-03-14 18:41:09 +0800

    mergeSort

--- Algorithm/NormalJs/sort.js
+++ Algorithm/NormalJs/sort.js
@@ -15,6 +15,24 @@ function quickSort(arr) {
     return [...quickSort(left), base, ...quickSort(right)]
 }
 
+function quickSortB(arr){
+    if(arr.length<=1) return arr
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+
+    for (let index = 0; index < arr.length -1; index++) {
+        const element = arr[index];
+        if (element>pivot) {
+            right.push(element)
+        }else{
+            left.push(element)
+        }   
+    }
+
+    return quickSort(left).concat([pivot],quickSort(right))
+}
+
 // bubble sort
 function bubbleSort(arr) {
     let len = arr.length;
@@ -86,4 +104,43 @@ function insertionSort(arr) {
     }
 
     return arr
+}
+
+
+function mergeSort(arr){
+ if (arr.length<2) {
+    return arr
+ }
+ let mid = Math.floor(arr.length/2)
+ let left = arr.slice(0,mid)
+ let right = arr.slice(mid)
+
+ return merge(mergeSort(left),mergeSort(right))
+}
+
+function merge(left,right){
+    let result=[]
+    let [i,j]=[0,0]
+
+    while(i<left.length && j<right.length){
+        if (left[i]<right[j]) {
+            result.push(left[i])
+            i++
+        }else{
+            result.push(right[j])
+            j++
+        }
+    }
+
+    while (i < left.length) {
+        result.push(left[i]);
+        i++;
+    }
+
+    while (j < right.length) {
+        result.push(right[j]);
+        j++;
+    }
+
+    return result
 }
