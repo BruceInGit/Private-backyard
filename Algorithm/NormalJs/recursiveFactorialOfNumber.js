c63c529d87263a5eedd411e27340946c1c4a43e5
Bruce Lee <bruce.withworld@gmail.com>
2025-04-08 18:31:19 +0800

    Climbing staircase

--- Algorithm/NormalJs/recursiveFactorialOfNumber.js
+++ Algorithm/NormalJs/recursiveFactorialOfNumber.js
@@ -1,5 +1,23 @@
-function recursiveFactorial(n){
-    if(n===0) return 1
+function recursiveFactorial(n) {
+    if (n === 0) return 1
 
-    return recursiveFactorial(n-1)*n
+    return recursiveFactorial(n - 1) * n
+}
+
+function climbingStaircase(n) {
+    let arr = [1, 2]
+
+    for (let index = 2; index < n; index++) {
+        arr[index] = arr[index - 1] + arr[index - 2];
+    }
+
+    return arr[n - 1]
+}
+
+function climbingStaircaseRecursive(n) {
+    if (n < 3) {
+        return n
+    }
+
+    return climbingStaircaseRecursive(n - 1) + climbingStaircaseRecursive(n - 2)
 }
