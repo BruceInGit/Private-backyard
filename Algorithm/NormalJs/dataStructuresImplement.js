9760d95dcd3c59c54a72b66047dfe709358eff75
Bruce Lee <bruce.withworld@gmail.com>
2025-04-25 18:38:59 +0800

    linked list

--- Algorithm/NormalJs/dataStructuresImplement.js
+++ Algorithm/NormalJs/dataStructuresImplement.js
@@ -117,3 +117,92 @@ class CircularQueue {
       console.log(result.toString());
    }
 }
+
+class Node {
+   constructor(value) {
+      this.value = value
+      this.next = null
+   }
+}
+
+class LinkedList {
+   constructor() {
+      this.head = null
+      this.size = 0
+   }
+
+   isEmpty() {
+      return this.size === 0
+   }
+
+   getSize() {
+      return this.size
+   }
+
+   print() {
+      if (this.isEmpty()) {
+         console.log("LinkedList is empty");
+         return 
+      }
+      let current = this.head
+      let linkedlistString = ""
+      while (current) {
+         if (current.next) {
+            linkedlistString += `${current.value} ->`
+         } else {
+            linkedlistString += `${current.value}`
+         }
+         current = current.next
+      }
+
+      console.log(linkedlistString)
+   }
+
+   append(value) {
+      const node = new Node(value)
+      if (this.isEmpty()) {
+         this.head = node
+      } else {
+         let prev = this.head
+         while (prev.next) {
+            prev = prev.next
+         }
+         prev.next = node
+      }
+      this.size++
+   }
+
+   removeAt(index){
+      if (index >= this.size || index < 0) {
+         console.log("Invalid index");
+         return null
+      }
+   
+      let removedNode;
+      if (index === 0) {
+        removedNode = this.head;
+        this.head = this.head.next;
+      } else {
+        let prev = this.head;
+        for (let i = 0; i < index - 1; i++) {
+          prev = prev.next;
+        }
+        removedNode = prev.next;
+        prev.next = removedNode.next;
+      }
+      
+      return removedNode
+   }
+
+   getAt(index){
+      if (index<0|| index >= this.size) {
+         return null
+      }
+      let current = this.head
+      for (let j = 0; j < index; j++) {
+       current = current.next
+      }
+    
+      return current.value
+   }
+}
